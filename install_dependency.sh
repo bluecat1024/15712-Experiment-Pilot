c87fda0b93882045b093517581d21cb2fd655500
Ubuntu <ubuntu@ip-172-31-92-14.ec2.internal>
2022-05-01 03:46:24 +0000

    add install_dependency set_env

---
+++ install_dependency.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install python3-pip
+sudo apt install gcc make libreadline8 libreadline-dev zlib1g zlib1g-dev build-essential bison postgresql-client llvm clang flex
+pip3 install psycopg2 pandas matplotlib
---
+++ set_environment.sh
@@ -0,0 +1,4 @@
+LD_LIBRARY_PATH=/usr/local/pgsql/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH
+PATH=/usr/local/pgsql/bin:$PATH
+export PATH
